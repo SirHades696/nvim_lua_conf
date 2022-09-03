758c2dde6c4bd87e9b1bf802c1d5214fd4714fd4
Emmanuel Nicanor <djnonasrm@gmail.com>
2022-09-03 12:08:16 -0500

    adding comments in snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,1 +1,9 @@
+-----------------------------------------------------------
+-- Friendly Snippets configuration file
+----------------------------------------------------------
+
+-- Plugin: friendly-snippets
+-- url: https://github.com/rafamadriz/friendly-snippets
+
+
 require("luasnip.loaders.from_vscode").lazy_load()
