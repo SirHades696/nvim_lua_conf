231b53623456ee2f701740b23765a6eee21ce962
Emmanuel Nicanor <djnonasrm@gmail.com>
2023-02-05 21:36:55 -0600

    Modify tokyonight theme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -25,11 +25,49 @@ end
 --require('onedark').load()
 
 -- Lua
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_comments = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "packer", "terminal", "Telescope", "Tagbar"}
-vim.cmd[[colorscheme tokyonight]]
+-- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_italic_functions = true
+-- vim.g.tokyonight_italic_comments = true
+-- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "packer", "terminal", "Telescope", "Tagbar"}
+-- vim.cmd[[colorscheme tokyonight]]
+
+color_scheme.setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help", "vista_kind", "packer", "terminal", "Telescope", "Tagbar" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+})
+
+color_scheme.load()
 
 -- statusline color schemes:
 -- import with: require('colors').colorscheme_name
